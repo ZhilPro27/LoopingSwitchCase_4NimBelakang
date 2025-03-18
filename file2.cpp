7f70800d8824c59c7a3b2eaf2d989e8db7a035f6
ZhilPro27 <zhilalkrisna@gmail.com>
2025-03-18 10:32:14 +0700

    first commit

---
+++ file2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+    int x;
+    int perulanganWhile = 0;
+    int perulanganDoWhile = 0;
+    // Perulangan while
+    cout << "perulangan dengan while" << endl;
+    srand(time(0));
+    x = rand() % 10;
+    cout << "Nilai awal x = " << x << endl;
+    while(x < 7){
+        cout << "nilai x = " << x << " Lari keliling lapangan!" << endl;
+        x = rand() % 10;
+        perulanganWhile++;
+    }
+    cout << "Nilai akhir x = " << x << endl;
+    cout << "Jumlah perulangan = " << perulanganWhile << endl;
+    cout << endl;
+    cout << "Perulangan dengan do while" << endl;
+    do{
+        cout << "nilai x = " << x << " Lari keliling lapangan!" << endl;
+        x = rand() % 10;
+        perulanganDoWhile++;
+    }while(x < 7);
+    cout << "Nilai akhir x = " << x << endl;
+    cout << "Jumlah perulangan = " << perulanganDoWhile << endl;
+}
